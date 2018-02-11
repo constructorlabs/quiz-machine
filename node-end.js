3a03bcbf28404bbd96772f7dfa9be5e5b09ea37a
Ania <ania.makes@gmail.com>
2018-02-11 17:39:57 +0000

    on answer, a new question is fetched and displayed. Also updates the inner gubbins such as true / flase, and removes the selection on radio button

--- node-end.js
+++ node-end.js
@@ -4,50 +4,94 @@ const app = express();
 const fetch = require('node-fetch');
 
 let currentQuestion = 0;
+let score = 0;
 
 app.use(bodyParser.json()); // parse incoming JSON
 app.use('/static', express.static('static'));
 app.set('view engine', 'hbs'); // use Handlebars as a view engine
 
 app.get("/quiz", function(req,res){
-  fetch('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple')
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(json){
-    let potentialAnswersArray = [].concat(json.results[0].incorrect_answers);
-    potentialAnswersArray.push(json.results[0].correct_answer);
-
-    console.log(potentialAnswersArray);
-    json.results[0].optionsToDisplay = randomiseAnswers(potentialAnswersArray);
-
-    potentialAnswersArray.forEach(function(item, index){
-      console.log(item);
-      let miniObject = {};
-      miniObject.answer = item;
-
-      if(item == json.results[0].correct_answer){
-        miniObject.correct = true;
-      } else {
-        miniObject.correct = false;
-      }
-
-      potentialAnswersArray[index] = miniObject;
-
-    });
-
-    console.log(json.results[0]);
-
-    res.render("display-question", json.results[0]);
+  getQuestion().then(function(questionJson){
+    res.render("display-question", questionJson);
   })
   .catch(function(error){
     res.status(500).json({error: "We failed our task"});
   });
 });
 
+
+var getQuestion = function () {
+  return new Promise(function(resolve, reject) {
+  fetch('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+
+
+      let potentialAnswersArray = [].concat(json.results[0].incorrect_answers);
+      potentialAnswersArray.push(json.results[0].correct_answer);
+
+      let optionsToDisplay = randomiseAnswers(potentialAnswersArray);
+
+      potentialAnswersArray.forEach(function(item, index) {
+        let miniObject = {};
+        miniObject.answer = item;
+
+        if (item == json.results[0].correct_answer) {
+          miniObject.correct = true;
+        } else {
+          miniObject.correct = false;
+        }
+
+        potentialAnswersArray[index] = miniObject;
+
+      });
+
+      let question =json.results[0].question;
+
+      console.log('have a question');
+      console.log(question);
+
+      let questionJson = {
+        question,
+        optionsToDisplay,
+      };
+
+      resolve(questionJson);
+    });
+});
+}
+
 app.listen(8080, function(){
   console.log("listening on port 8080");
 });
+
+app.post("/answer", function(req, res, next){
+  console.log("/answer body:", req.body);
+
+  if (req.body.answer) {
+    score += 1;
+  } else {
+    score = 0;
+  }
+
+
+  getQuestion().then(function(question){
+
+
+    res.json({
+      score,
+      question,
+    });
+  });
+
+
+
+});
+
+
+
 
 function randomiseAnswers(arrayOfAnswers){
   var currentIndex = arrayOfAnswers.length,
--- static/browser-end.js
+++ static/browser-end.js
@@ -9,13 +9,65 @@ function sendInfo(event){
   console.log(answerList);
   var isTrue = (checkForSelection() == 'true')
 
+
+
+
+  postData('http://localhost:8080/answer', {answer: isTrue})
+    .then(function(data){
+
+      console.log(data)
+      console.log(data.question)
+
+      removeSelection();
+
+      document.getElementById("question-text").innerHTML = data.question.question;
+
+      var optionsToDisplayArray = data.question.optionsToDisplay;
+
+      for (let i = 0; i< optionsToDisplayArray.length; i++){
+        document.getElementById(`label${i}`).innerHTML = optionsToDisplayArray[i].answer;
+        document.getElementById(`answer${i}`).value = optionsToDisplayArray[i].correct;
+      }
+
+  }) // JSON from `response.json()` call
+    .catch(error => console.error(error))
+
+
+
+
   if(isTrue){
-    alert ("correct");
+    console.log("correct");
+    // tell the server we have correct answer
+    // create a fetch to report the correct answer (increment points and fetch a new question)
+
+    // create a route ON SERVER that will increment the score and fetch another question and return the question in JSON format - simple res.JSON in second then
+
   } else {
-    alert ("wrong. Boo");
+    console.log("wrong. Boo");
   }
 
 }
+
+
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *same-origin
+    redirect: 'follow', // *manual, error
+    referrer: 'no-referrer', // *client
+  })
+  .then(response => response.json()
+) // parses response to JSON
+}
+
 
 function checkForSelection(){
   var itemValue;
@@ -28,4 +80,13 @@ function checkForSelection(){
   });
 
   return itemValue;
+}
+
+
+function removeSelection(){
+  Array.from(answerList).forEach(function(item){
+    if(item.checked){
+      item.checked = false;
+    }
+  });
 }
