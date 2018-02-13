3d2274bb96abb80c89f5e51e014afc25ae9fe21f
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-02-13 02:16:20 +0000

    functioning quiz-machine

--- server.js
+++ server.js
@@ -8,43 +8,88 @@ const storage = {
   currentQuestion: null,
 }
 
+
 app.use(bodyParser.json());
 
 app.set('view engine', 'hbs');
 app.use('/static', express.static('static'));
 
 
-app.get('/temp', function(req, res) {
-  fetch('https://opentdb.com/api.php?amount=1')
+
+app.get('/quiz', function(req,res) {
+  fetch('https://opentdb.com/api.php?amount=1&type=multiple')
   .then(function(response){
     return response.json();
   }).then(function(json){
     let data = json.results[0];
-    res.render('quizViewer', data);
+    res.render('mainPage', data);
   }).catch(function(error){
     res.status(500).json({ error: 'We failed to fetch the question' });
   });
 });
 
-app.get('/', function(req, res) {
-  res.render('mainPage', req.body);
-});
+
+// app.get('/get-next-question', function(req, res) {
+//
+// })
 
 
-app.get('/get-question', function(req, res) {
-  if (storage.currentQuestion === null) {
-    fetch('https://opentdb.com/api.php?amount=1')
+app.get('/check-answer/:answer', function(req, res) {
+  let answer = req.params.answer;
+  if (answer === "correct") {
+    storage.counter += 1;
+    // get client to display counter
+    //fetch new question from API
+    fetch('https://opentdb.com/api.php?amount=1&type=multiple')
     .then(function(response){
       return response.json();
-    }).then(function(data){
-      res.json(data.results[0]);
-      storage.currentQuestion = data.results[0];
+    }).then(function(json){
+      let data = json.results[0];
+      data.counter = storage.counter
+      res.json(data);
+    }).catch(function(error){
+      res.status(500).json({ error: 'We failed to fetch the question' });
     });
+  } else if (answer === "wrong") {
+    storage.counter = 0;
+    fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+    .then(function(response){
+      return response.json();
+    }).then(function(json){
+      let data = json.results[0];
+      data.counter = storage.counter
+      res.json(data);
+    }).catch(function(error){
+      res.status(500).json({ error: 'We failed to fetch the question' });
+    });
+    //do something else
   }
-  else {
-    res.json(storage.currentQuestion);
-  }
-});
+})
+
+
+
+
+
+//
+// app.get('/', function(req, res) {
+//   res.render('mainPage', req.body);
+// });
+
+
+// app.get('/get-question', function(req, res) {
+//   if (storage.currentQuestion === null) {
+//     fetch('https://opentdb.com/api.php?amount=1')
+//     .then(function(response){
+//       return response.json();
+//     }).then(function(data){
+//       res.json(data.results[0]);
+//     });
+//   }
+//   else {
+//     res.json(storage.currentQuestion);
+//   }
+// });
+
 
 //app.post('/answer-question', ...)
 //param = answer
@@ -78,4 +123,23 @@ app.get('/get-question', function(req, res) {
 
 app.listen(8080, function(){
   console.log('I am listening on port 8080');
+});
+
+
+
+
+
+
+/// Maybe no longer needed
+app.get('/temp', function(req, res) {
+  fetch('https://opentdb.com/api.php?amount=1')
+  .then(function(response){
+    return response.json();
+  }).then(function(json){
+    let data = json.results[0];
+    data.counter = storage.counter
+    res.render('quizViewer', data);
+  }).catch(function(error){
+    res.status(500).json({ error: 'We failed to fetch the question' });
+  });
 });
--- static/client.js
+++ static/client.js
@@ -1,37 +1,91 @@
-function getNextQuestion() {
-  //no params
-  //performs fetch to webserver to get json object with question data
-  //doesn't return anything
-  //calls displayQuestion function with question data
+let answerGroup = document.getElementById('formWithAnswers');
+
+
+function displayQuestionAndAnswers(displayData) {
+  document.getElementById('welcomeHeader').innerHTML = '';
+  document.getElementById('rightWrong').innerHTML = '';
+  document.getElementById('question').innerHTML = displayData.question;
+  document.getElementById('formWithAnswers').innerHTML = '';
+
+  let newCorrect = document.createElement('button');
+  newCorrect.setAttribute('id', 'correct');
+  newCorrect.setAttribute('class', 'btn btn-lg btn-block')
+  newCorrect.innerHTML = displayData.correct_answer;
+  answerGroup.appendChild(newCorrect);
+
+  let newWrong1 = document.createElement('button');
+  newWrong1.setAttribute('id', 'wrong');
+  newWrong1.setAttribute('class', 'btn btn-lg btn-block')
+  newWrong1.innerHTML = displayData.incorrect_answers[0];
+  answerGroup.appendChild(newWrong1);
+
+  let newWrong2 = document.createElement('button');
+  newWrong2.setAttribute('id', 'wrong');
+  newWrong2.setAttribute('class', 'btn btn-lg btn-block')
+  newWrong2.innerHTML = displayData.incorrect_answers[1];
+  answerGroup.appendChild(newWrong2);
+
+  let newWrong3 = document.createElement('button');
+  newWrong3.setAttribute('id', 'wrong');
+  newWrong3.setAttribute('class', 'btn btn-lg btn-block')
+  newWrong3.innerHTML = displayData.incorrect_answers[2];
+  answerGroup.appendChild(newWrong3);
+
+  document.getElementById('points').innerHTML = "Your point score is" + " " + displayData.counter;
+ }
+
+
+
+answerGroup.addEventListener("click", checkAnswerAndGetNewQuestion);
+
+function checkAnswerAndGetNewQuestion(event) {
+  //listens for submit on buttons with answers
+  //fetch from webserver (answer input)
+  //if correct answer: 1. fetch request to webserver to up the counter 2.display counter with new points (DOM manipulation) 3. calls getNextQuestion
+  //if wrong answer: 1. fetch request to webserver to reset counter 2.calls getNextQuestion
+  event.preventDefault();
+
+  let answer = event.target;
+  if (answer.getAttribute('id') === 'correct') {
+    document.getElementById('rightWrong').innerHTML = "CORRECT!";
+
+    fetch('/check-answer/correct')
+    .then(function(response){
+      return response.json();
+    }).then(function(newData){
+      //do something
+      displayQuestionAndAnswers(newData);
+    })
+  }
+  else if (answer.getAttribute('id') === 'wrong'){
+    document.getElementById('rightWrong').innerHTML = "OH NO! Wrong answer!";
+    fetch('/check-answer/wrong')
+    .then(function(response){
+      return response.json();
+    }).then(function(data){
+      displayQuestionAndAnswers(data);
+      //do something
+    })
+  }
+
 }
 
-getNextQuestion();
-
-function displayQuestion() {
-  //param: question data
-  //identify question in question data
-  //identify answers in question data
-  //sort and arrange answers
-  //DOM manipulation to display question and answers
-  //doesn't return anything
-}
-
-function listenForAnswer() {
-  //no params
-  //event listener
-  //listens for submit on radio buttons with answers
-  //app post to webserver (answer)
-  //if correct answer: 1. post to webserver to up the counter 2.display counter with new points (DOM manipulation) 3. calls getNextQuestion
-  //if wrong answer: 1. post to webserver to reset counter 2.calls getNextQuestion
-}
 
 
-fetch('http://localhost:8080/get-question')
-.then(function(response){
-  return response.json();
-}).then(function(data){
-  console.log(data);
-})
+
+
+
+
+
+
+//Working fetch
+
+// fetch('http://localhost:8080/get-question')
+// .then(function(response){
+//   return response.json();
+// }).then(function(data){
+//   console.log(data);
+// })
 
 
 
@@ -45,3 +99,44 @@ fetch('http://localhost:8080/get-question')
 //   //calls getNextQuestion function to get next question
 // }
 // listenForGetNextQuestion();
+
+
+// function displayQuestionAndAnswers(data) {
+//   //param: question data
+//   //identify question in question data
+//   //identify answers in question data
+//   //sort and arrange answers
+//   //DOM manipulation to display question and answers
+//   //doesn't return anything
+//
+//
+//   console.log(data);
+//   questionString = data.question;
+//   data['incorrect_answers'].forEach(answer => answerOptionsObject.answer = false;
+//   answerOptionsObject.data['correct_answer'] = true;
+//   //answerArray.push(data['correct_answer']);
+//   console.log(answerOptionsObject);
+//
+//   let block = document.createElement('div');
+//   block.innerHTML = `<h3>${questionString}</h3>`;
+//   display.appendChild(block);
+//
+// }
+
+
+
+// function getQuestion() {
+//   //no params
+//   //performs fetch to webserver to get json object with question data
+//   //doesn't return anything
+//   //calls displayQuestion function with question data
+//   fetch('http://localhost:8080/get-question')
+//   .then(function(response){
+//     return response.json();
+//   }).then(function(data){
+//     displayQuestionAndAnswers(data);
+//
+//   });
+// }
+
+//getQuestion();
