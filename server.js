1234eb245e8ff8fbd623c629104bf06d8f60bf83
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-04-23 19:27:33 +0100

    readme

--- server.js
+++ server.js
@@ -34,15 +34,6 @@ app.get('/quiz', function(req,res) {
     return response.json();
   }).then(function(json){
     let data = json.results[0];
-    // console.log(data);
-    //
-    // let question = data.question;
-    // let answers = incorrect_answers;
-    //
-    //
-    //
-    // console.log(answers)
-
     res.render('mainPage', data);
   }).catch(function(error){
     res.status(500).json({ error: 'We failed to fetch the question' });
@@ -105,20 +96,6 @@ app.get('/check-answer/:answer', function(req, res) {
 })
 
 
-
-
-
-
-// app.get('/get-next-question', function(req, res) {
-//
-// })
-
-//
-// app.get('/', function(req, res) {
-//   res.render('mainPage', req.body);
-// });
-
-
 app.get('/get-question', function(req, res) {
   if (storage.currentQuestion === null) {
     fetch('https://opentdb.com/api.php?amount=1&type=multiple')
@@ -137,37 +114,9 @@ app.get('/get-question', function(req, res) {
 });
 
 
-//app.post('/answer-question', ...)
-//param = answer
-//checks if answer is correct
-//resets currentQuestion
-//ups counter if correct
-//resets counter if wrong
-//res Yay/Nay + points
-
-
-// app.post('/', function(req, res) {
-//   fetch('https://opentdb.com/api.php?amount=1')
-//   .then(function(response){
-//     return response.json();
-//   }).then(function(json){
-//     let data = json.results[0];
-//     let question = data.question;
-//     res.render('quizViewer', data);
-//   }).catch(function(error){
-//     res.status(500).json({ error: 'We failed to fetch the question' });
-//   });
-// })
-
-
-
 app.listen(8080, function(){
   console.log('I am listening on port 8080');
 });
-
-
-
-
 
 
 /// Maybe no longer needed
